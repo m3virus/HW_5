fda70a04fd1a68ae41f5a41758438359a38ac4ef
mohammad_hasan <mryazdani98@outlook.com>
2023-06-07 18:27:15 +0330

    repositorys added

---
+++ ConsoleApp1/ConsoleApp1/interface/IProdcutRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp1.domain;
+
+namespace ConsoleApp1
+{
+    public interface IProdcutRepository
+    {
+        
+
+    }
+}
+
