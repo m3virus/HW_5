276e411078e862adfb8f1e3d20508274b007297f
mohammad_hasan <mryazdani98@outlook.com>
2023-06-07 20:56:22 +0330

    adding repositorys rule

--- ConsoleApp1/ConsoleApp1/interface/IStockRepository.cs
+++ ConsoleApp1/ConsoleApp1/interface/IStockRepository.cs
@@ -4,11 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ConsoleApp1.domain;
+using System.Text.Json;
+using System.IO;
+
 
 namespace ConsoleApp1
 {
     interface IStockRepository
     {
-
+        string SaleProduct(int productId, int cnt);
+        string BuyProduct(Stock productInStock);
+        List<Stock> GetSalesProductList();
     }
 }
---
+++ ConsoleApp1/ConsoleApp1/interface/ProductRepository.cs
@@ -0,0 +1,27 @@
+﻿using ConsoleApp1.domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class ProductRepository : IProdcutRepository
+    {
+        public string AddProduct(Product product)
+        {
+            
+        }
+
+        public string GetProductByID(int ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetProductList()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
