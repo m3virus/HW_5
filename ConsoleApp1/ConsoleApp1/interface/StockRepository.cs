50e70f1297f273f0e868bc9108ca35b36e69b5ff
mohammad_hasan <mryazdani98@outlook.com>
2023-06-08 01:36:43 +0330

    stock repository completed

--- ConsoleApp1/ConsoleApp1/interface/StockRepository.cs
+++ ConsoleApp1/ConsoleApp1/interface/StockRepository.cs
@@ -7,6 +7,7 @@ using System.Text.Json;
 using System.IO;
 using ConsoleApp1.domain;
 using Newtonsoft.Json;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace ConsoleApp1
 {
@@ -26,63 +27,120 @@ namespace ConsoleApp1
         }
         public string BuyProduct(Stock productInStock)
         {
-            
+            try
+            {
+                var mainStock = new Stock();
+                string fileName = "StockJson.json";
+                var jsonData = File.ReadAllText(fileName);
+                List<Stock> stockList = JsonConvert.DeserializeObject<List<Stock>>(jsonData);
+                foreach(var i in stockList)
+                {
+                    if(i.ProductID == productInStock.ProductID)
+                    {
+                        mainStock = i;
+                        stockList.Remove(i);
+                        break;
+                    }
+                }
+                mainStock.ProductPrice = ((mainStock.ProductPrice * mainStock.ProductQuantity) + (productInStock.ProductPrice * productInStock.ProductQuantity)) / (productInStock.ProductQuantity + mainStock.ProductQuantity) ;
+                mainStock.ProductQuantity += productInStock.ProductQuantity;
+                stockList.Add(mainStock);
+                string jsonDataUpdate = JsonConvert.SerializeObject(stockList, Formatting.Indented);
+                File.WriteAllText(fileName, jsonDataUpdate);
+
+                return "the product is buying";
+                
+
+            }
+            catch (Exception FileEx)
+            {
+
+                return FileEx.Message;
+            }
         }
 
         public List<Stock> GetSalesProductList()
         {
-            throw new NotImplementedException();
+            try
+            {
+                string fileName = "StockJson.json";
+                var jsonData = File.ReadAllText(fileName);
+                List<Stock> stockList = JsonConvert.DeserializeObject<List<Stock>>(jsonData);
+                string txtPath = "../salesproduct";
+                FileStream stream = new FileStream(txtPath, FileMode.OpenOrCreate);
+                BinaryFormatter formatter = new BinaryFormatter();
+
+                
+                formatter.Serialize(stream, stockList);
+
+                return stockList;
+                
+            }
+            catch (Exception FileEx)
+            {
+
+                Console.WriteLine(FileEx.Message);
+                return null;
+            }
         }
 
         public string SaleProduct(int productId, int amount)
         {
-            int check = 0;
-            Stock mainStock = new Stock();
-            string fileName = "StockJson.json";
-            var jsonData = File.ReadAllText(fileName);
-            List<Stock> stockList = JsonConvert.DeserializeObject<List<Stock>>(jsonData);
+            try
+            {
+                int check = 0;
+                Stock mainStock = new Stock();
+                string fileName = "StockJson.json";
+                var jsonData = File.ReadAllText(fileName);
+                List<Stock> stockList = JsonConvert.DeserializeObject<List<Stock>>(jsonData);
 
-            foreach(var i in stockList)
-            {
-                if(i.ProductID == productId)
+                foreach (var i in stockList)
                 {
-                    mainStock = i;
-                    stockList.Remove(mainStock);
-                    check++;
-                    break;
+                    if (i.ProductID == productId)
+                    {
+                        mainStock = i;
+                        stockList.Remove(i);
+                        check++;
+                        break;
+                    }
                 }
-            }
-            if (check == 1)
-            {
-                var difference = GetProductQuantity(mainStock.ProductQuantity, amount);
-                if (difference == -1)
+                if (check == 1)
                 {
-                    stockList.Add(mainStock);
-                    string jsonDataUpdate = JsonConvert.SerializeObject(stockList, Formatting.Indented);
-                    File.WriteAllText(fileName, jsonDataUpdate);
-                    return "we dont have enought amount ";
-                }
-                else if(difference == 0)
-                {
-                    string jsonDataUpdate = JsonConvert.SerializeObject(stockList, Formatting.Indented);
-                    File.WriteAllText(fileName, jsonDataUpdate);
-                    return Convert.ToString(difference);
+                    var difference = GetProductQuantity(mainStock.ProductQuantity, amount);
+                    if (difference == -1)
+                    {
+                        stockList.Add(mainStock);
+                        string jsonDataUpdate = JsonConvert.SerializeObject(stockList, Formatting.Indented);
+                        File.WriteAllText(fileName, jsonDataUpdate);
+                        return "we dont have enought amount ";
+                    }
+                    else if (difference == 0)
+                    {
+                        string jsonDataUpdate = JsonConvert.SerializeObject(stockList, Formatting.Indented);
+                        File.WriteAllText(fileName, jsonDataUpdate);
+                        return Convert.ToString(difference);
+                    }
+                    else
+                    {
+                        mainStock.ProductQuantity = difference;
+                        stockList.Add(mainStock);
+                        string jsonDataUpdate = JsonConvert.SerializeObject(stockList, Formatting.Indented);
+                        File.WriteAllText(fileName, jsonDataUpdate);
+                        return Convert.ToString(difference);
+
+                    }
                 }
                 else
                 {
-                    mainStock.ProductQuantity = difference;
-                    stockList.Add(mainStock);
-                    string jsonDataUpdate = JsonConvert.SerializeObject(stockList, Formatting.Indented);
-                    File.WriteAllText(fileName, jsonDataUpdate);
-                    return Convert.ToString(difference);
-                    
+                    return "this product is unvalid";
                 }
             }
-            else
+            catch (Exception FileEx)
             {
-                return "this product is unvalid";
+
+                return FileEx.Message;
             }
-            s
+            
 
         }
     }
